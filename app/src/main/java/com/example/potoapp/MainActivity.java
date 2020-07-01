f14aa0f04610c17eb081c517ce45ae4215a534cb
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-01 23:27:17 +0900

    7/01 activity_main

--- app/src/main/java/com/example/potoapp/MainActivity.java
+++ app/src/main/java/com/example/potoapp/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
     }
 }
